9ef5da2a9d4bf068038cbdef398765eb0409968d
Xavier <nicodemexavier@gmail.com>
2022-02-08 15:53:16 +0100

    exercice 4

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,9 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Form\ProductType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use DateTime;
 
 /**
  * @Route("/product", name="product_")
@@ -21,6 +25,28 @@ class ProductController extends AbstractController
         $products = $repository->findAll();
         return $this->render('product/index.html.twig', [
             'products' => $products,
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="new")
+     */
+    public function new(Request $request, ManagerRegistry $managerRegistry): Response
+    {
+        $product = new Product();
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $product->setCreationDate(new DateTime());
+            $managerRegistry->getManager()->persist($product);
+
+            $managerRegistry->getManager()->flush();
+
+            return $this->redirectToRoute('product_index');
+        }
+        return $this->render('product/new.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
